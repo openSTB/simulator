131b9ededd400695b410aab7f1bd3644b0cb51cf
Blair Bonnett <blair.bonnett@hsu-hh.de>
2024-08-07 12:31:36 +0200

    Start a registry of plugin loaders.
    A function which loads a plugin class can be registered with the @register_loader
    decorator. If a function has already been registered, it will be replaced.

--- src/openstb/simulator/plugin/loader.py
+++ src/openstb/simulator/plugin/loader.py
@@ -4,12 +4,13 @@
 import hashlib
 import importlib.metadata
 import importlib.util
+import inspect
 from pathlib import Path
-from typing import Literal, cast, overload
+from typing import Callable, Literal, cast, overload
 
 from openstb.i18n.support import domain_translator
 from openstb.simulator.plugin import abc
-from openstb.simulator.types import PluginOrSpec
+from openstb.simulator.types import F_PluginLoader, PluginOrSpec, T_Plugin
 
 
 _ = domain_translator("openstb.simulator")
@@ -149,7 +150,101 @@ def load_plugin_class(group: str, name: str) -> type[abc.Plugin]:
     return getattr(mod, classname)
 
 
+#: Lookup table of functions which have been registered as able to load a particular
+#: class of plugin.
+registered_loaders: dict[abc.Plugin, F_PluginLoader] = {}
+
+
+# For when the decorator is used directly (without parameters).
+@overload
+def register_loader(__loader: F_PluginLoader[T_Plugin]) -> F_PluginLoader[T_Plugin]: ...
+
+
+# For when the decorator is used with parameters.
+@overload
+def register_loader(
+    *, docstring: bool | None = None, name: str | None = None
+) -> Callable[[F_PluginLoader[T_Plugin]], F_PluginLoader[T_Plugin]]: ...
+
+
+def register_loader(
+    __loader: F_PluginLoader[T_Plugin] | None = None,
+    *,
+    docstring: bool | None = None,
+    name: str | None = None,
+):
+    """Decorator to register a plugin loading function.
+
+    This should be applied to a function which takes a
+    `openstb.simulator.types.PluginOrSpec` parameter and returns a plugin instance.
+
+    Parameters
+    ----------
+    docstring : Boolean, optional
+        Whether to set a docstring for the class. If None (the default), then a
+        docstring will only be set if one does not already exist.
+    name : str, optional
+        The name of the plugin type to use in the docstring. If not specified, the
+        class name will be used.
+
+    """
+
+    def registration(loader: F_PluginLoader[T_Plugin]) -> F_PluginLoader[T_Plugin]:
+        # Use the return annotation to do the actual registration.
+        cls = inspect.signature(loader).return_annotation
+        registered_loaders[cls] = loader
+
+        # Check if we need to set a doctring.
+        if docstring is None:
+            set_docstring = loader.__doc__ is None or len(loader.__doc__.strip()) == 0
+        else:
+            set_docstring = docstring
+        if set_docstring:
+            docname = name or cls.__name__
+            loader.__doc__ = f"""Load a {docname} plugin.
+
+Parameters
+----------
+plugin_spec : dict, {cls.__name__}
+    If a dictionary, this specifies the name and parameters of the {docname} to
+    load. Otherwise, it is assumed to be an instance of a compatible class and is
+    returned unchanged.
+
+Returns
+-------
+{cls.__name__}"""
+
+        return loader
+
+    # Directly used.
+    if __loader is not None:
+        return registration(__loader)
+
+    # Used with parameters.
+    return registration
+
+
 def load_plugin(group: str, plugin_spec: PluginOrSpec[abc.Plugin]) -> abc.Plugin:
+    """Load a plugin and create an instance of it.
+
+    In general, you should call one of the more specific functions in this module to
+    load the particular type of plugin you want.
+
+    Parameters
+    ----------
+    group : str
+        The name of the entry point group that the plugin belongs to, e.g.,
+        "openstb.simulator.trajectory".
+    plugin_spec : dict, Plugin
+        If a dictionary, this specifies the name and parameters of the plugin to
+        load. Otherwise, it is assumed to be an instance of a compatible class and is
+        returned unchanged.
+
+    Returns
+    -------
+    abc.Plugin
+
+    """
     # Note: we cannot check isinstance(..., Mapping) here as plugins may implement the
     # Mapping interface.
     if isinstance(plugin_spec, dict):
@@ -164,34 +259,40 @@ def load_plugin(group: str, plugin_spec: PluginOrSpec[abc.Plugin]) -> abc.Plugin
 # about the return type.
 
 
+@register_loader(name="configuration loader")
 def config_loader(plugin_spec: PluginOrSpec[abc.ConfigLoader]) -> abc.ConfigLoader:
     return cast(
         abc.ConfigLoader, load_plugin("openstb.simulator.config_loader", plugin_spec)
     )
 
 
+@register_loader(name="Dask cluster")
 def dask_cluster(plugin_spec: PluginOrSpec[abc.DaskCluster]) -> abc.DaskCluster:
     return cast(
         abc.DaskCluster, load_plugin("openstb.simulator.dask_cluster", plugin_spec)
     )
 
 
+@register_loader(name="environmental parameters")
 def environment(plugin_spec: PluginOrSpec[abc.Environment]) -> abc.Environment:
     return cast(
         abc.Environment, load_plugin("openstb.simulator.environment", plugin_spec)
     )
 
 
+@register_loader(name="ping times")
 def ping_times(plugin_spec: PluginOrSpec[abc.PingTimes]) -> abc.PingTimes:
     return cast(abc.PingTimes, load_plugin("openstb.simulator.ping_times", plugin_spec))
 
 
+@register_loader(name="point targets")
 def point_targets(plugin_spec: PluginOrSpec[abc.PointTargets]) -> abc.PointTargets:
     return cast(
         abc.PointTargets, load_plugin("openstb.simulator.point_targets", plugin_spec)
     )
 
 
+@register_loader(name="result converter")
 def result_converter(
     plugin_spec: PluginOrSpec[abc.ResultConverter],
 ) -> abc.ResultConverter:
@@ -201,52 +302,45 @@ def result_converter(
     )
 
 
+@register_loader(name="multiplicative scale factor")
 def scale_factor(plugin_spec: PluginOrSpec[abc.ScaleFactor]) -> abc.ScaleFactor:
     return cast(
         abc.ScaleFactor, load_plugin("openstb.simulator.scale_factor", plugin_spec)
     )
 
 
+@register_loader(name="signal")
 def signal(plugin_spec: PluginOrSpec[abc.Signal]) -> abc.Signal:
     return cast(abc.Signal, load_plugin("openstb.simulator.signal", plugin_spec))
 
 
+@register_loader(name="signal window")
 def signal_window(plugin_spec: PluginOrSpec[abc.SignalWindow]) -> abc.SignalWindow:
-    """Load a signal window plugin.
-
-    Parameters
-    ----------
-    plugin_spec : PluginSpec
-        If a dictionary, this specifies the name and parameters of the signal window to
-        load. Otherwise, it is assumed to be an instance of a compatible class and is
-        returned unchanged.
-
-    Returns
-    -------
-    signal_window : openstb.simulator.abc.SignalWindow
-
-    """
     return cast(
         abc.SignalWindow, load_plugin("openstb.simulator.signal_window", plugin_spec)
     )
 
 
+@register_loader(name="system")
 def system(plugin_spec: PluginOrSpec[abc.System]) -> abc.System:
     return cast(abc.System, load_plugin("openstb.simulator.system", plugin_spec))
 
 
+@register_loader(name="trajectory")
 def trajectory(plugin_spec: PluginOrSpec[abc.Trajectory]) -> abc.Trajectory:
     return cast(
         abc.Trajectory, load_plugin("openstb.simulator.trajectory", plugin_spec)
     )
 
 
+@register_loader(name="transducer")
 def transducer(plugin_spec: PluginOrSpec[abc.Transducer]) -> abc.Transducer:
     return cast(
         abc.Transducer, load_plugin("openstb.simulator.transducer", plugin_spec)
     )
 
 
+@register_loader(name="travel time")
 def travel_time(plugin_spec: PluginOrSpec[abc.TravelTime]) -> abc.TravelTime:
     return cast(
         abc.TravelTime, load_plugin("openstb.simulator.travel_time", plugin_spec)
--- src/openstb/simulator/types.py
+++ src/openstb/simulator/types.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 
 from collections.abc import Mapping
-from typing import Any, TypedDict, TypeVar, Union
+from typing import Any, Callable, TypedDict, TypeVar, Union
 
 from openstb.simulator.plugin.abc import Plugin
 
@@ -24,3 +24,7 @@ T_Plugin = TypeVar("T_Plugin", bound=Plugin)
 #: Either a plugin, or a plugin specification dictionary.
 #: This can be specialised, e.g., PluginOrSpec[abc.Signal].
 PluginOrSpec = Union[T_Plugin, PluginSpec]
+
+
+#: A callable which takes a plugin or specification and returns a plugin.
+F_PluginLoader = Callable[[PluginOrSpec[T_Plugin]], T_Plugin]
