f3cd42c246100a53a8cf61298a095fa753c63b69
Blair Bonnett <blair.bonnett@hsu-hh.de>
2024-08-09 09:50:45 +0200

    Add __main__.py so the CLI can be run with python -m openstb.simulator

---
+++ src/openstb/simulator/__main__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: openSTB contributors
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+
+from openstb.simulator.cli import openstb_sim
+
+
+if __name__ == "__main__":
+    openstb_sim()
