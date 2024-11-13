b934833e0402844f252785ebaa11b5dbd19c78a9
Blair Bonnett <blair.bonnett@hsu-hh.de>
2024-11-13 12:09:13 +0100

    Add iterative (non-stop-and-hop) travel time calculator.

--- src/openstb/simulator/travel_time.py
+++ src/openstb/simulator/travel_time.py
@@ -7,12 +7,17 @@ import numpy as np
 from numpy.typing import ArrayLike
 import quaternionic
 
+from openstb.i18n.support import domain_translator
 from openstb.simulator.plugin.abc import (
     Environment,
     Trajectory,
     TravelTime,
     TravelTimeResult,
 )
+from openstb.simulator.util import rotate_elementwise
+
+
+_ = domain_translator("openstb.simulator", plural=False)
 
 
 class StopAndHop(TravelTime):
@@ -79,6 +84,133 @@ class StopAndHop(TravelTime):
             rx_orientation=rx_ori,
             rx_velocity=rx_vel,
             rx_vector=rx_vec,
+            rx_path_length=rx_pathlen,
+            scale_factor=None,
+        )
+
+
+class Iterative(TravelTime):
+    """Travel time including vehicle motion during the ping.
+
+    This uses an iterative approach to find the travel time. The initial guess is found
+    with the assumption that the vehicle motion at the start of the ping is constant
+    throughout the ping. The receiver position at the proposed reception time is found.
+    From the corresponding path length an updated travel time is found. This continues
+    to iterate until successive travel times are within the requested tolerance.
+
+    This uses the sound speed at the position of the vehicle and time of the ping
+    transmission throughout the calculation, and assumes that the sound travels in a
+    straight line to and from the targets.
+
+    Note that this plugin does not calculate any scale factors to be applied. To model
+    effects such as attenuation and spreading loss, separate plugins must be included in
+    the simulation setup.
+
+    """
+
+    def __init__(self, max_iterations: int, tolerance: float):
+        """
+        Parameters
+        ----------
+        max_iterations : int
+            If a solution is not found within this many iterations, an exception will be
+            raised.
+        tolerance : float
+            The tolerance in seconds. If the travel time estimated in successive
+            iterations differ by less than this, they are considered successful.
+
+        """
+        self.max_iterations = max_iterations
+        self.tolerance = tolerance
+
+    def calculate(
+        self,
+        trajectory: Trajectory,
+        ping_time: float,
+        environment: Environment,
+        tx_position: ArrayLike,
+        tx_orientation: ArrayLike | quaternionic.QArray,
+        rx_positions: ArrayLike,
+        rx_orientations: ArrayLike | quaternionic.QArray,
+        target_positions: ArrayLike,
+    ) -> TravelTimeResult:
+        # Find the position and orientation of the vehicle at the start of the ping.
+        vehicle_pos0 = trajectory.position(ping_time)
+        vehicle_vel0 = trajectory.velocity(ping_time)
+        vehicle_speed0 = np.linalg.norm(vehicle_vel0)
+        vehicle_ori0 = trajectory.orientation(ping_time)
+
+        # Sound speed at transmission.
+        sound_speed = environment.sound_speed(ping_time, vehicle_pos0)
+
+        # Transmitter details at the start of the ping.
+        tx_pos = vehicle_pos0 + vehicle_ori0.rotate(np.asarray(tx_position))
+        tx_ori = quaternionic.array(tx_orientation) * vehicle_ori0
+
+        rx_positions = np.asarray(rx_positions)
+        rx_orientations = quaternionic.array(rx_orientations)
+
+        # Vector and range between receivers and targets at start of ping.
+        target_positions = np.asarray(target_positions)  # (Nt, 3)
+        rx_vec0 = target_positions - rx_positions[:, np.newaxis, :]  # (Nr, Nt, 3)
+        rx_r0 = np.linalg.norm(rx_vec0, axis=-1)  # (Nr, Nt)
+
+        # Initial estimate of travel time using the linear velocity approximation.
+        vrel = (rx_vec0 * vehicle_vel0).sum(axis=-1)  # (Nr, Nt)
+        tt = (2 * rx_r0 * sound_speed - 2 * vrel) / (sound_speed**2 - vehicle_speed0**2)
+
+        # Transmit portion.
+        tx_vec = target_positions - tx_pos
+        tx_pathlen = np.sqrt(np.sum(tx_vec**2, axis=-1))
+        tx_vec /= tx_pathlen[:, np.newaxis]
+        tx_tt = tx_pathlen / sound_speed  # (Nt,)
+
+        # Loop for the maximum number of iterations; exit if we converge sooner.
+        success = False
+        for i in range(self.max_iterations):
+            # Times the echoes would be received at with current estimate.
+            rx_time = ping_time + tt  # (Nr, Nt)
+
+            # Position and orientation of the vehicle at proposed reception times.
+            vehicle_pos_rx = trajectory.position(rx_time)  # (Nr, Nt, 3)
+            vehicle_ori_rx = trajectory.orientation(rx_time)  # (Nr, Nt, 4)
+
+            # Corresponding position of receivers.
+            rx_pos_rx = vehicle_pos_rx + rotate_elementwise(
+                vehicle_ori_rx, rx_positions[:, np.newaxis, :]
+            )  # (Nr, Nt, 3)
+
+            # Vector and path length for each echo to proposed receiver positions.
+            rx_vec = rx_pos_rx - target_positions
+            rx_pathlen = np.sqrt(np.sum(rx_vec**2, axis=-1))
+
+            # Total travel time for these receiver positions.
+            new_tt = tx_tt + rx_pathlen / sound_speed
+
+            # If it is close to the previous travel time (which was used to find the
+            # receiver positions), we have converged to a solution.
+            dt = np.abs(new_tt - tt)
+            tt = new_tt
+            if np.all(dt < self.tolerance):
+                success = True
+                break
+
+        # Check why we exited the loop.
+        if not success:
+            raise RuntimeError(_("could not converge to a travel time"))
+
+        # Put it all together.
+        return TravelTimeResult(
+            travel_time=tt,
+            tx_position=tx_pos,
+            tx_orientation=tx_ori,
+            tx_velocity=vehicle_vel0,
+            tx_vector=tx_vec,
+            tx_path_length=tx_pathlen,
+            rx_position=rx_pos_rx,
+            rx_orientation=rx_orientations[:, np.newaxis] * vehicle_ori_rx,
+            rx_velocity=trajectory.velocity(ping_time + tt),
+            rx_vector=rx_vec / rx_pathlen[..., np.newaxis],
             rx_path_length=rx_pathlen,
             scale_factor=None,
         )
--- tests/test_travel_time.py
+++ tests/test_travel_time.py
@@ -1,9 +1,15 @@
 # SPDX-FileCopyrightText: openSTB contributors
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 
-import numpy as np
+from datetime import datetime
 
-from openstb.simulator import environment, travel_time
+import numpy as np
+import pytest
+import quaternionic
+
+from openstb.simulator import travel_time
+from openstb.simulator.environment import InvariantEnvironment
+from openstb.simulator.plugin.abc import Trajectory
 from openstb.simulator.system.trajectory import Linear
 
 
@@ -16,9 +22,7 @@ def test_tt_stopandhop():
     result = ttcalc.calculate(
         traj,
         10.0,
-        environment.InvariantEnvironment(
-            salinity=35, sound_speed=1500.0, temperature=9.6
-        ),
+        InvariantEnvironment(salinity=35, sound_speed=1500.0, temperature=9.6),
         [0, 1, 0],
         [np.cos(np.pi / 4), 0, 0, np.sin(np.pi / 4)],
         [[0, 1, 0], [0, 1, -1], [0, 1, 1]],
@@ -44,7 +48,7 @@ def test_tt_stopandhop():
     assert np.allclose(
         np.array(result.rx_orientation), [np.sqrt(2) / 2, 0, 0, np.sqrt(2) / 2]
     )
-    assert result.rx_velocity.shape == (1, 1, 3) # not (3, 2, 3) but broadcastable.
+    assert result.rx_velocity.shape == (1, 1, 3)  # not (3, 2, 3) but broadcastable.
     assert np.allclose(result.rx_velocity, [1, 0, 0])
     assert np.allclose(result.rx_path_length, [[29, 39], [l0, l1], [l0, l1]])
     assert np.allclose(
@@ -65,3 +69,105 @@ def test_tt_stopandhop():
         ],
     )
     assert result.scale_factor is None
+
+
+def test_tt_iterative_linear_colocated():
+    """travel_time: iterative calculation, colocated system + linear trajectory"""
+    c = 1478.0
+    v = 1.0
+
+    ttcalc = travel_time.Iterative(max_iterations=20, tolerance=50e-9)
+    traj = Linear([-10, 0, 0], [10, 0, 0], v)
+    result = ttcalc.calculate(
+        traj,
+        10.0,
+        InvariantEnvironment(salinity=18, sound_speed=c, temperature=4.2),
+        [0, 1, 0],
+        [np.cos(np.pi / 4), 0, 0, np.sin(np.pi / 4)],
+        [[0, 1, 0]],
+        [[np.cos(np.pi / 4), 0, 0, np.sin(np.pi / 4)]],
+        [[0, 30, 0], [0, 40, 0]],
+    )
+
+    # Check transmitter details.
+    assert result.tx_position.shape == (3,)
+    assert np.allclose(result.tx_position, [0, 1, 0])
+    assert result.tx_orientation.shape == (4,)
+    assert np.allclose(
+        np.array(result.tx_orientation), [np.sqrt(2) / 2, 0, 0, np.sqrt(2) / 2]
+    )
+    assert result.tx_velocity.shape == (3,)
+    assert np.allclose(result.tx_velocity, [1, 0, 0])
+    assert result.tx_vector.shape == (2, 3)
+    assert np.allclose(result.tx_vector, [0, 1, 0])
+    assert result.tx_path_length.shape == (2,)
+    assert np.allclose(result.tx_path_length, [29, 39])
+
+    # True travel time for the linear velocity.
+    dotprod = np.sum(result.tx_velocity[np.newaxis, :] * result.tx_vector, axis=-1)
+    twtt = (2 * result.tx_path_length * c - dotprod) / (c**2 - v**2)
+    assert result.travel_time.shape == (1, 2)
+    assert np.allclose(result.travel_time, twtt)
+
+    # Orientation and velocity are constant.
+    assert result.rx_orientation.shape == (1, 2, 4)
+    assert np.allclose(
+        np.array(result.rx_orientation), [np.sqrt(2) / 2, 0, 0, np.sqrt(2) / 2]
+    )
+    assert result.rx_velocity.shape == (1, 2, 3)
+    assert np.allclose(result.rx_velocity, [1, 0, 0])
+
+    # Receiver positions should differ.
+    assert result.rx_position.shape == (1, 2, 3)
+    assert np.allclose(result.rx_position, [[twtt[0] * v, 1, 0], [twtt[1] * v, 1, 0]])
+    assert result.rx_vector.shape == (1, 2, 3)
+
+    # Path length and direction.
+    rvec = np.array([[twtt[0] * v, -29, 0], [twtt[1] * v, -39, 0]])
+    rlen = np.linalg.norm(rvec, axis=-1)
+    assert result.rx_path_length.shape == (1, 2)
+    assert np.allclose(result.rx_path_length, rlen)
+    assert result.rx_vector.shape == (1, 2, 3)
+    assert np.allclose(result.rx_vector, rvec / rlen[:, np.newaxis])
+
+
+class _RandomPositionTraj(Trajectory):
+    duration = 10.0
+    length = 10.0
+    start_time = datetime.now()
+
+    def __init__(self, seed):
+        self.rng = np.random.default_rng(seed)
+
+    def position(self, t):
+        shape = list(np.array(t).shape) + [3]
+        return self.rng.uniform(-10, 10, shape)
+
+    def orientation(self, t):
+        shape = list(np.array(t).shape) + [4]
+        ori = np.zeros(shape, dtype=float)
+        ori[..., 0] = 1
+        return quaternionic.array(ori)
+
+    def velocity(self, t):
+        shape = list(np.array(t).shape) + [3]
+        vel = np.zeros(shape, dtype=float)
+        vel[..., 0] = 1
+        return vel
+
+
+def test_tt_iterative_nonconverging():
+    """travel_time: iterative calculation reports non-convergence"""
+    ttcalc = travel_time.Iterative(max_iterations=20, tolerance=50e-9)
+    traj = _RandomPositionTraj(1178191)
+    with pytest.raises(RuntimeError, match="could not converge"):
+        ttcalc.calculate(
+            traj,
+            10.0,
+            InvariantEnvironment(salinity=18, sound_speed=1492.4, temperature=4.2),
+            [0, 1, 0],
+            [np.cos(np.pi / 4), 0, 0, np.sin(np.pi / 4)],
+            [[0, 1, 0]],
+            [[np.cos(np.pi / 4), 0, 0, np.sin(np.pi / 4)]],
+            [[0, 30, 0], [0, 40, 0]],
+        )
