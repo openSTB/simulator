ab262f7d54eeb4e416bafafce004d0555475471a
Blair Bonnett <blair.bonnett@hsu-hh.de>
2025-02-07 11:19:06 +0100

    Split the result converter plugins into their own modules.

---
+++ src/openstb/simulator/result_converter/numpy.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: openSTB contributors
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+
+import os
+from pathlib import Path
+from typing import Any
+
+import numpy as np
+
+from openstb.i18n.support import domain_translator
+from openstb.simulator.plugin.abc import ResultConverter, ResultFormat, SimulationConfig
+
+
+_ = domain_translator("openstb.simulator", plural=False)
+
+
+class NumpyConverter(ResultConverter):
+    """Convert simulation results to a NumPy file.
+
+    The writes the data as a NumPy .npz file with the following variables:
+    * ``baseband_frequency``: the frequency used to baseband the data
+    * ``fill_value``: value used to fill any missing samples in the results
+    * ``ping_start_time``: seconds since the start of the trajectory that the pings
+      were sent
+    * ``pressure``: the simulated pressure at each receiver
+    * ``pressure_dimensions``: string array giving the order of dimensions in
+      ``pressure``.
+    * ``sample_time``: seconds since the start of its ping that each sample was captured
+
+    Parameters
+    ----------
+    filename : path-like
+        The path to save the converted results at.
+    compress : Boolean
+        If True, compress the data while writing.
+
+    """
+
+    def __init__(self, filename: os.PathLike[str] | str, compress=False):
+        self.filename = Path(filename)
+        self.compress = compress
+        if self.filename.exists():
+            raise ValueError(
+                _("output file {filename} already exists").format(
+                    filename=self.filename
+                )
+            )
+
+    def can_handle(self, format: ResultFormat | str, config: SimulationConfig):
+        return format == ResultFormat.ZARR_BASEBAND_PRESSURE
+
+    def convert(
+        self, format: ResultFormat | str, result: Any, config: SimulationConfig
+    ):
+        if format != ResultFormat.ZARR_BASEBAND_PRESSURE:
+            return False
+
+        rdict = {
+            "ping_start_time": result["ping_start_time"][:],
+            "sample_time": result["sample_time"][:],
+            "pressure": result["pressure"][:],
+            "pressure_dimensions": ["ping", "channel", "sample_time"],
+            "baseband_frequency": result.attrs.baseband_frequency,
+            "fill_value": result.attrs.fill_value,
+        }
+
+        if self.compress:
+            np.savez_compressed(self.filename, **rdict)
+        else:
+            np.savez(self.filename, **rdict)
+
+        return True
